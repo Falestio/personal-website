066d1c146f7795a59e76c7af5c205c767020bbc7
Falestio <falestioltmpt@gmail.com>
2022-06-22 14:46:49 +0700

    add download link

--- pages/index.vue
+++ pages/index.vue
@@ -7,8 +7,10 @@ const allPost = await queryContent('/').find()
         <div class="c pt-14">
             <h1 class="text-h1 text-center mb-2">Falestio.dev</h1>
             <p class="text-base text-center">Blog web development</p>
-            
-    
+            <a class="text-base link" href="../assets/pdf/Tubes2_Falestio Hanif Al Hakim.pdf" download>
+                FILE TUTORIAL WEB HOSTING
+            </a>
+        
             <div class="artikel-terbaru flex flex-col gap-8 mt-8">
                 <div v-for="post in allPost" :key="post.title">
                     <NuxtLink :to="post._path" class="block p-6 border-b-2 border-slate-600">
